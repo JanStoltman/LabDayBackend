f2d26a4ef73d5282da7e204befeeb46621e39bc7
Jan Stoltman <stoltmanjan@gmail.com>
2018-02-05 18:38:44 +0100

    Local settings fix

---
+++ LabDayBackend/LabDayBackend/local_settings.py
@@ -0,0 +1,7 @@
+# Requests over HTTP are redirected to HTTPS.
+SECURE_SSL_REDIRECT = False
+
+# This instructs the browser to only send these cookies over
+# HTTPS connections.
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
